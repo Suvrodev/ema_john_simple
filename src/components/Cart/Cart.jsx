88409bbddf2da486b80bfde56bb3c2c2b0bbbf20
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-03-30 00:36:03 +0600

    Display Quantity in the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,16 +7,21 @@ const Cart = ({cart}) => {
     let Total=0;
     let Total_Shipping=0;
     let Tax=0;
+    let quantity=0;
+
     for(const product of cart){
-        Total=Total+product.price;
+        if(product.quantity===0) product.quantity=1;
+       // product.quantity=product.quantity||1;
+        Total=Total+product.price* product.quantity;
         Total_Shipping+=product.shipping;
+        quantity=quantity+product.quantity;
     }
     Tax=((Total*7)/100);
     const grandTotal=Total+Total_Shipping+Tax;
     return (
         <div className='cart'>
             <h4>Order Summery</h4>
-            <p>Selected Items: {cart.length} </p>
+            <p>Selected Items: {quantity} </p>
             <p>Total Price:$ {Total} </p>
             <p>Total Shipping: ${Total_Shipping} </p>
             <p>Tax: ${Tax.toFixed(2)} </p>
