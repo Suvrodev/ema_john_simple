6b643b5d38369cc83de096c3d389f841bedbb7ae
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-03-29 21:14:07 +0600

    Added Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Header.css'
+import Header_Logo from '../../Copy/images/Logo.svg'
 
 const Header = () => {
     return (
         <div>
-            <h2>This is Header</h2>
+            <nav className="header">
+                <img src={Header_Logo} alt="" />
+                <div>
+                    <a href="/Shop">Shop</a>
+                    <a href="/Order">Order</a>
+                    <a href="/Inventory">Inventory</a>
+                    <a href="Login">Login</a>
+                </div>
+            </nav>
         </div>
     );
 };
